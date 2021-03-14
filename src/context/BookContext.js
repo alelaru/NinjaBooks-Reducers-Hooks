c7c25f3bb809ec55084eab45ba0c85a6af269119
alelaru <alelaru@gmail.com>
2021-03-15 00:16:25 +0100

    We use Reduce and use localStorage to get the info in the browser

--- src/context/BookContext.js
+++ src/context/BookContext.js
@@ -1,10 +1,19 @@
-import React, { createContext, useReducer, useState } from 'react';
+import React, { createContext, useEffect, useReducer } from 'react';
 import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, dispatch] = useReducer(bookReducer,[])
+    const [books, dispatch] = useReducer(bookReducer,[], () => {
+        const localData = localStorage.getItem("books")
+        // It returns the value as books
+        return localData? JSON.parse(localData) : []
+    })
+    // You use it great because it will change the localStorage everytime there is a change in the books
+    // For example Add, Delete or Edit
+    useEffect(() => {
+        localStorage.setItem("books", JSON.stringify(books))
+    },[books])
 
     return ( 
         <BookContext.Provider value={{ books, dispatch}}>
